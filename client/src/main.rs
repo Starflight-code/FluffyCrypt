c946bc13ce7c2cf83018b812b0c34830d79ee09f
mikkejos22-dev <mikkelsonj@uwplatt.edu>
2024-11-18 17:55:48 -0600

    Update main.rs- joey

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,30 @@
+use tokio::fs::{ DirEntry, read_dir };
+use tokio::task;
+use std::io;
+use std::path::Path;
+
+async fn list_files(dir_path: &str) -> io::Result<Vec<DirEntry>> {
+    let mut files: Vec<DirEntry> = Vec::new();
+    let mut entries = read_dir(Path::new(dir_path)).await?;
+
+    while let Some(entry) = entries.next_entry().await? {
+        files.push(entry);
+    }
+
+    Ok(files)
+}
+
 #[tokio::main]
 async fn main() {
-    println!("Hello, world!");
-}
+    let dir_path = "C:\\Users\\mikke";
+    match list_files(dir_path).await {
+        Ok(files) => {
+            for file in files {
+                let file_name = file.file_name();
+                let file_name_str = file_name.to_string_lossy().into_owned();
+                println!("{}", file_name_str);
+                        }
+                    }
+                    Err(e) => eprintln!("Error: {}", e),
+                }
+            }
