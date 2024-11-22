c4e157c3adbb937df41c4e4495d5d1c879dc754b
Ben Kobiske <kobiskeb@uwplatt.edu>
2024-11-21 22:00:41 -0600

    Add: Compile time bundling of the public key

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,9 @@
+#[cfg(unix)]
+const PUB_KEY: &[u8] = include_bytes!("../pub.key");
+
+#[cfg(windows)]
+const PUB_KEY: &[u8] = include_bytes!("..\\pub.key");
+
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
