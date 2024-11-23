f26db29f08f03071c71e73b781acd88dc0a128dd
Ben Kobiske <kobiskeb@uwplatt.edu>
2024-11-23 11:42:04 -0600

    Add: file recurse method Adds a method to recurse the filesystem from a certain folder, to all subdirectories

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,8 +1,35 @@
+use std::fs::DirEntry;
+use std::path::{Path, PathBuf};
+use std::vec::Vec;
+
 #[cfg(unix)]
 const PUB_KEY: &[u8] = include_bytes!("../pub.key");
 
 #[cfg(windows)]
 const PUB_KEY: &[u8] = include_bytes!("..\\pub.key");
+
+fn recurse_directory(path: PathBuf) -> Option<Vec<DirEntry>> {
+    let mut files = Vec::new();
+    if path.read_dir().is_err() {
+        return None;
+    }
+    for file in path.read_dir().unwrap() {
+        if file.is_err() {
+            continue;
+        }
+        let file = file.unwrap();
+
+        if file.path().is_dir() {
+            let new_files = recurse_directory(file.path());
+            if let Some(mut recursed) = new_files {
+                files.append(&mut recursed);
+            }
+        } else if file.path().is_file() {
+            files.push(file);
+        }
+    }
+    return Some(files);
+}
 
 #[tokio::main]
 async fn main() {
