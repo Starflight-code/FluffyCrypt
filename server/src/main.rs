943d284f361bf776fad32d36a8b87164f89331f3
mikkejos22-dev <mikkelsonj@uwplatt.edu>
2024-12-11 16:41:39 -0600

    Fixed bug Made it so the key had size limits

--- server/src/main.rs
+++ server/src/main.rs
@@ -45,7 +45,6 @@ fn shift_u64_to_i64(number: u64) -> i64 {
 async fn handle_message(msg: Message<'_>, socket: &TcpStream) {
     use crate::schema::asymmetric_key::dsl as asym_dsl;
     use crate::schema::client_key::dsl as client_dsl;
-
     let mut db = establish_connection().await;
     match msg {
         Message::RegisterClient((id, recieved_key)) => {
@@ -82,7 +81,11 @@ async fn handle_message(msg: Message<'_>, socket: &TcpStream) {
                     encryption_key: decoded,
                     paid: false,
                 };
-
+                if socket.writable().await.is_ok() {
+                if(decoded.len() < 246 && decoded.len() > 266) {
+                    let _ = socket.try_write(&Message::Malformed().to_req().to_vec());
+                }
+            }
                 insert_into(client_dsl::client_key)
                     .values(record)
                     .execute(&mut db)
